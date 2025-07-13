cfa8c9e139b412224c051f354da59b158646e589
Valery Tengu Sarutobi Ilychev <ilychev@gmail.com>
2025-07-13 12:54:44 +0300

    Корректировка логотипа

--- src/sopds/urls/local.py
+++ src/sopds/urls/local.py
@@ -6,13 +6,4 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 from .base import urlpatterns
 
-#urlpatterns = [
-#    path('opds/', include('opds_catalog.urls', namespace='opds')),
-#    path('web/', include('sopds_web_backend.urls', namespace='web')),
-#    path('admin/', admin.site.urls),
-#    #url(r'^logout/$', logout, {'next_page':'/web/'},name='logout'),   
-#    #url(r'^', include('sopds_web_backend.urls', namespace='web', app_name='opds_web_backend')),
-#    path('', RedirectView.as_view(url=reverse_lazy("web:main"))),
-#] + debug_toolbar_urls()
-
 urlpatterns += debug_toolbar_urls()
