211900383e21c7d6a7d45d82528375b75a9741a6
Valery Tengu Sarutobi Ilychev <ilychev@gmail.com>
2025-07-27 11:13:17 +0300

    Корректировки файлов лицензии

--- src/opds_catalog/settings.py
+++ src/opds_catalog/settings.py
@@ -4,8 +4,6 @@ from django.conf import settings
 from django.utils import translation
 from constance import config
 
-#import environ
-
 loglevels={'debug':logging.DEBUG,'info':logging.INFO,'warning':logging.WARNING,'error':logging.ERROR,'critical':logging.CRITICAL,'none':logging.NOTSET}
 NOZIP_FORMATS = ['epub', 'mobi']
 
