249c43271684339916cbdd1480f814b4e8380c61
Valery A. Ilychev <valery.ilychev@yandex.ru>
2025-10-26 16:04:18 +0300

    Линтинг файла настроек

--- src/opds_catalog/settings.py
+++ src/opds_catalog/settings.py
@@ -1,52 +1,71 @@
 import logging
-import os
-from django.conf import settings
-from django.utils import translation
-from constance import config
 
-loglevels={'debug':logging.DEBUG,'info':logging.INFO,'warning':logging.WARNING,'error':logging.ERROR,'critical':logging.CRITICAL,'none':logging.NOTSET}
-NOZIP_FORMATS = ['epub', 'mobi']
+# from constance import config
+from constance.signals import config_updated
+from django.conf import settings
+
+# import os
+from django.db.backends.signals import connection_created
+from django.dispatch import receiver
+from django.utils import translation
+
+loglevels = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL,
+    "none": logging.NOTSET,
+}
+NOZIP_FORMATS = ["epub", "mobi"]
 
 VERSION = getattr(settings, "VERSION", "develop")
 TITLE = getattr(settings, "SOPDS_TITLE", "SimpleOPDS NG")
-SUBTITLE = getattr(settings, "SOPDS_SUBTITLE", "SimpleOPDS Catalog new generation by www.sopds.ru. Version %s."%VERSION)
+SUBTITLE = getattr(
+    settings,
+    "SOPDS_SUBTITLE",
+    "SimpleOPDS Catalog new generation by www.sopds.ru. Version %s." % VERSION,
+)
 ICON = getattr(settings, "SOPDS_ICON", "/static/images/favicon.ico")
 THUMB_SIZE = 300
 
 loglevel = getattr(settings, "SOPDS_LOGLEVEL", "info")
 if loglevel.lower() in loglevels:
-    LOGLEVEL=loglevels[loglevel.lower()]
+    LOGLEVEL = loglevels[loglevel.lower()]
 else:
-    LOGLEVEL=logging.NOTSET
-    
-from django.dispatch import receiver
-from constance.signals import config_updated
+    LOGLEVEL = logging.NOTSET
+
 
 @receiver(config_updated)
 def constance_updated(sender, key, new_value, old_value, **kwargs):
-    if key == 'SOPDS_LANGUAGE':
+    if key == "SOPDS_LANGUAGE":
         translation.activate(new_value)
-        print(new_value)
+        # print(new_value)
+
 
 def constance_update_all():
     pass
-    
+
+
 # Переопределяем некоторые функции для SQLite, которые работают неправлено
-from django.db.backends.signals import connection_created
+
 
 def sopds_upper(s):
     return s.upper()
 
-def sopds_substring(s,i,l):
-    i = i - 1
-    return s[i:i+l]
 
-def sopds_concat(s1='',s2='',s3=''):
-    return "%s%s%s"%(s1,s2,s3)
+def sopds_substring(s, i, l):
+    i = i - 1
+    return s[i : i + l]
+
+
+def sopds_concat(s1="", s2="", s3=""):
+    return "%s%s%s" % (s1, s2, s3)
+
 
 @receiver(connection_created)
 def extend_sqlite(connection=None, **kwargs):
     if connection.vendor == "sqlite":
-        connection.connection.create_function('upper',1,sopds_upper)
-        connection.connection.create_function('substring',3,sopds_substring)
-        connection.connection.create_function('concat',3,sopds_concat)
+        connection.connection.create_function("upper", 1, sopds_upper)
+        connection.connection.create_function("substring", 3, sopds_substring)
+        connection.connection.create_function("concat", 3, sopds_concat)
