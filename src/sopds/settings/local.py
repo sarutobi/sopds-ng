239cb06214736bd8bbc67f506748faa5fa593bf0
Valery A. Ilychev <valery.ilychev@yandex.ru>
2025-10-30 09:18:24 +0300

    Для предоставления статических файлов используется whitenoise
    Настройка whitenoise для сжатия и версионирования статических файлов.
    Линтинг конфигурационных файлов

--- src/sopds/settings/local.py
+++ src/sopds/settings/local.py
@@ -1,18 +1,18 @@
-from .base import *
+from .base import *  # noqa: F403
 import socket
 
 DEBUG = True
 
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
+INSTALLED_APPS += ("debug_toolbar",)  # noqa: F405
 
-MIDDLEWARE += [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+MIDDLEWARE += [  # noqa: F405
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-ROOT_URLCONF = 'sopds.urls.local'
+ROOT_URLCONF = "sopds.urls.local"
 
-INTERNAL_IPS = [ '127.0.0.1',]
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 ip = socket.gethostbyname(socket.gethostname())
-INTERNAL_IPS += [ip[:-1] + '1']
+INTERNAL_IPS += [ip[:-1] + "1"]
