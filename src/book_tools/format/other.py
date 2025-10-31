45a3dcf13bb9824a744b0fa8dedfaa3c4ec2ac12
Valery A. Ilychev <valery.ilychev@yandex.ru>
2025-10-31 14:11:39 +0300

    Переименование файла с тестами парсеров fb2

--- src/book_tools/format/other.py
+++ src/book_tools/format/other.py
@@ -11,8 +11,10 @@ class Dummy(BookFile):
 
 class Dummy_new(object):
     def __init__(self, file, original_filename, mimetype):
+        pass
+
+    def parse_book_data(self, file, original_filename, mimetype) -> BookFile:
         return BookFile(file, original_filename, mimetype)
 
     def __exit__(self, kind, value, traceback):
         pass
-
