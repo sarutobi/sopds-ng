35ecf6b129f1fd6ba2d0015ab450324397b58818
Valery A. Ilychev <valery.ilychev@yandex.ru>
2025-11-03 12:59:51 +0300

    Актуализация теста fb2sax_new
    Тест использует механизм сравнения объектов вместо сравнения по полям

--- src/book_tools/tests/format/test_fb2_parsers.py
+++ src/book_tools/tests/format/test_fb2_parsers.py
@@ -79,19 +79,7 @@ def test_fb2sax_new(test_rootlib, book, expected_exception) -> None:
     with expected_exception:
         book_actual = FB2sax(file, "Test Book")
         book_new = FB2sax_new(file, "Test Book").parse_book_data(file, "Test Book")
-        assert book_actual is not None
-        assert book_new is not None
-
-        assert book_actual.file == book_new.file
-        assert book_actual.mimetype == book_new.mimetype
-        assert book_actual.original_filename == book_new.original_filename
-        assert book_actual.title == book_new.title
-        assert book_actual.description == book_new.description
-        assert book_actual.authors == book_new.authors
-        assert book_actual.tags == book_new.tags
-        assert book_actual.series_info == book_new.series_info
-        assert book_actual.language_code == book_new.language_code
-        assert book_actual.issues == book_new.issues
+        assert book_actual == book_new
         assert book_actual.docdate == book_new.docdate
 
 
