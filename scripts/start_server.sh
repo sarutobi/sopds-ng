2629a6d4ee6ea4b0b9b0ffbf5499a1f80d210003
Valery A. Ilychev <valery.ilychev@yandex.ru>
2026-02-01 13:02:34 +0300

    испр: Корректировка скрпта запуска сервера
    В скрипт добавлена переменная окружения DJANGO_SETTINGS_MODULE
    Для команд uv run добавлены флаги --no-dev для исключения синхронизации
    venv под окружение разработчика.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export DJANGO_SETTINGS_MODULE='sopds.settings.base'
 # Create log directory
 mkdir -p log
 
@@ -8,13 +9,13 @@ uv sync --no-dev
 
 # Create key
 if [ ! -f 'secret_key.txt' ]; then
-    uv run python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())' >secret_key.txt
+    uv run --no-dev python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())' >secret_key.txt
 fi
 # Collect statics files
-uv run manage.py collectstatic --skip-checks --no-input
+uv run --no-dev manage.py collectstatic --skip-checks --no-input
 
 # Run DB migrations
-uv run manage.py migrate --skip-checks --no-input
+uv run --no-dev manage.py migrate --skip-checks --no-input
 
 # Run server
-uv run --env-file=.env gunicorn --config="python:sopds.settings.gunicorn" sopds.wsgi
+uv run --no-dev --env-file=.env gunicorn --config="python:sopds.settings.gunicorn" sopds.wsgi
